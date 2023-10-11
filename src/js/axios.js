c71055a885fe3e8cb09cd652306a80b367877f73
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-10-11 12:03:31 +0300

    axios test 2

--- src/js/axios.js
+++ src/js/axios.js
@@ -7,7 +7,7 @@ const searchValue = 'cat';
 const BASE_URL = 'https://pixabay.com/api/';
 const AUTH_KEY = '39799120-0adfdb8bf4f296c3a7d41d46c';
 
-// function axios ====
+// function axios
 
 async function getResponseArr() {
   const resp = await axios.get(`${BASE_URL}?key=${AUTH_KEY}`, {
