bc1b64aa0d596b4b9f8adbc4d9be3678721bb356
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-10-11 09:12:48 +0300

    axios

---
+++ src/js/axios.js
@@ -0,0 +1,27 @@
+import axios from 'axios';
+
+const perPage = 10;
+const currentPage = 1;
+const searchValue = 'cat';
+
+const BASE_URL = 'https://pixabay.com/api/';
+const AUTH_KEY = '39799120-0adfdb8bf4f296c3a7d41d46c';
+
+//==  Функція HTTP-запита на бекенд для пошуку зображень за ключовим словом
+
+async function getResponseArr() {
+  const resp = await axios.get(`${BASE_URL}?key=${AUTH_KEY}`, {
+    params: {
+      q: `${searchValue}`,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: true,
+      per_page: `${perPage}`,
+      page: `${currentPage}`,
+    },
+  });
+
+  const arr = await resp.data;
+  return arr;
+}
+console.log(getResponseArr());
