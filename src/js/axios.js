795d7f5e6ae28b9a832575644c4d75b03446e39b
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-10-11 11:26:30 +0300

    axios

--- src/js/axios.js
+++ src/js/axios.js
@@ -7,8 +7,6 @@ const searchValue = 'cat';
 const BASE_URL = 'https://pixabay.com/api/';
 const AUTH_KEY = '39799120-0adfdb8bf4f296c3a7d41d46c';
 
-//==  Функція HTTP-запита на бекенд для пошуку зображень за ключовим словом
-
 async function getResponseArr() {
   const resp = await axios.get(`${BASE_URL}?key=${AUTH_KEY}`, {
     params: {
