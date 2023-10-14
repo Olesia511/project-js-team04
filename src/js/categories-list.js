2ec3bd232194d90214d3cbe4585d6fd9ee936c51
AnatoliiZhurov <129226759+AnatoliiZhurov@users.noreply.github.com>
2023-10-14 21:06:27 +0300

    Click

--- src/js/categories-list.js
+++ src/js/categories-list.js
@@ -2,28 +2,57 @@ import { getCategoryListArr } from './axios';
 
 const categoriesList = document.querySelector(`.categories-list`);
 
-console.log(categoriesList);
+let curentCategory = null;
 
 getCategoryListArr()
   .then(res => {
-    const html = res
-      .map(
-        ({ list_name }) => `<li class="list-categories-item">${list_name}</li>`
-      )
-      .join(``);
+    const html =
+      `<li class="list-categories-item">All categories</li>` +
+      res
+        .map(
+          ({ list_name }) =>
+            `<li class="list-categories-item">${list_name}</li>`
+        )
+        .join(``);
     console.log(`html`, html);
 
     categoriesList.insertAdjacentHTML(`beforeend`, html);
     console.log(`getCategoryListArr`, res);
     const firstElementList = categoriesList.firstChild;
     const firstText = firstElementList.textContent;
+    // console.log('firstElementList', firstElementList.classList);
+    curentCategory = firstElementList;
+    console.log(curentCategory);
 
-    const upperCaseText = firstText.toUpperCase();
+    addCurrent();
+    // const upperCaseText = firstText.toUpperCase();
 
-    firstElementList.textContent = upperCaseText;
+    // firstElementList.textContent = upperCaseText;
+    // firstElementList.style.color = 'var(--violet-color)';
   })
   .catch(rej => console.log(`rej`, rej));
 
 categoriesList.addEventListener(`click`, categotySelect);
 
-function categotySelect(evt) {}
+function categotySelect(evt) {
+  console.log(evt.currentTarget);
+  if (evt.target === curentCategory) {
+    return;
+  } else if (evt.target === evt.currentTarget) {
+    return;
+  } else if (evt.target === categoriesList.firstChild) {
+  }
+  removeCurrent();
+
+  curentCategory = evt.target;
+  console.log(curentCategory);
+  curentCategory.classList.add(`active-list-item`);
+}
+
+function addCurrent() {
+  curentCategory.classList.add(`active-list-item`);
+}
+
+function removeCurrent() {
+  curentCategory.classList.remove(`active-list-item`);
+}
