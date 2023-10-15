7be961d37da625702c60762ba882c1d9945995a2
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-10-15 12:54:44 +0000

    Deploying to gh-pages from @ Olesia511/project-js-team04@77f9f97831a399f737d76d189406020cc0921fc4 🚀

--- assets/shopping-list-38a2e5fd.js
+++ assets/shopping-list-38a2e5fd.js
@@ -1,12 +1,12 @@
-import"./sup-ukraine-4b886f61.js";const i=document.querySelector(".shop-list-list"),l=document.querySelector(".shop-list-placeholder"),c="user-books";function m(){const o=document.querySelector(".js-description"),t=o.textContent;if(window.innerWidth<768){const e=t.slice(0,86)+"…";o.textContent=e}else if(window.innerWidth>=768&&window.innerWidth<1440){const e=t.slice(0,253)+"…";o.textContent=e}else o.textContent=t}const n=JSON.parse(localStorage.getItem(c));function r(o){const t=o.map(({buy_links:e="null",amazon_product_url:s="null",book_image:a="null",title:p="null",list_name:d="null",description:g="null",author:h,_id:u="null"})=>`<li class="shop-list-item" data-id="${u}">
+import"./sup-ukraine-2c6e1869.js";const l=document.querySelector(".shop-list-list"),c=document.querySelector(".shop-list-placeholder"),r="user-books";function m(){const o=document.querySelector(".js-description"),t=o.textContent;if(window.innerWidth<768){const e=t.slice(0,86)+"…";o.textContent=e}else if(window.innerWidth>=768&&window.innerWidth<1440){const e=t.slice(0,253)+"…";o.textContent=e}else o.textContent=t}const n=JSON.parse(localStorage.getItem(r));function a(o){const t=o.map(({buy_links:e="null",amazon_product_url:s="null",book_image:p="null",title:i="null",list_name:d="null",description:g="null",author:h,_id:u="null"})=>`<li class="shop-list-item" data-id="${u}">
               <button type="button" class="delete-btn">
                 <svg class="delete-btn-icon" width="16" height="16">
                   <use href="./img/icon.svg#icon-shop-list-delete"></use>
                 </svg>
               </button>
-              <div class="book-cover">${a}</div>
+              <div class="book-cover"><img class="book-cover-img" src="${p}" alt="${i}" /></div>
               <div class="book-info">
-                <h2 class="book-name">${p}</h2>
+                <h2 class="book-name">${i}</h2>
                 <p class="book-category">${d}</p>
                 <p class="book-description js-description">${g}</p>
                 <p class="book-author">${h}</p>
@@ -48,4 +48,4 @@ import"./sup-ukraine-4b886f61.js";const i=document.querySelector(".shop-list-lis
                   </a>
                 </div>
               </div>
-        </li>`).join("");n.length!==0?(l.hidden=!0,i.innerHTML=t,m(),document.querySelectorAll(".delete-btn").forEach(s=>{s.addEventListener("click",f)})):(l.hidden=!1,i.innerHTML="")}r(n);function f(o){const t=o.target.closest(".shop-list-item").dataset.id,e=n.findIndex(s=>s._id===t);e!==-1&&(n.splice(e,1),localStorage.setItem(c,JSON.stringify(n)),r(n))}
+        </li>`).join("");n.length!==0?(c.hidden=!0,l.innerHTML=t,m(),document.querySelectorAll(".delete-btn").forEach(s=>{s.addEventListener("click",b)})):(c.hidden=!1,l.innerHTML="")}a(n);function b(o){const t=o.target.closest(".shop-list-item").dataset.id,e=n.findIndex(s=>s._id===t);e!==-1&&(n.splice(e,1),localStorage.setItem(r,JSON.stringify(n)),a(n))}
