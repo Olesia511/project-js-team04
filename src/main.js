db99da7d13a241522185d44bbb50a381e7190579
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-10-16 22:53:58 +0300

    test

--- src/main.js
+++ src/main.js
@@ -4,7 +4,6 @@ import './js/categories-list.js';
 import './js/category-book.js';
 import './js/create-markup.js';
 import './js/header.js';
-import './js/home.js';
 import './js/markup-by-category.js';
 import './js/mobile-menu.js';
 import './js/modal.js';
