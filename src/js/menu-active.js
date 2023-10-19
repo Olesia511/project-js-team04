f53d436ea1d1a66f1f9736e39ac76ac6ff836dec
Olesia511 <131859369+Olesia511@users.noreply.github.com>
2023-10-19 08:59:26 +0300

    test

--- src/js/menu-active.js
+++ src/js/menu-active.js
@@ -1,3 +1,36 @@
+// const currentPageHash = window.location.href;
+
+// const headerLinkHome = document.querySelector('.site-nav-item-home');
+// const headerLinkShop = document.querySelector('.site-nav-item-shop');
+// const firstLinkMobile = document.querySelector('.home-target');
+// const secondLinkMobile = document.querySelector('.shop-target');
+
+// function setActivePage() {
+//   if (currentPageHash.includes('index')) {
+//     headerLinkHome.classList.add('active');
+//     headerLinkShop.classList.remove('active');
+//     firstLinkMobile.classList.add('active');
+//     secondLinkMobile.classList.remove('active');
+//   } else if (currentPageHash.includes('shopping-list')) {
+//     headerLinkHome.classList.remove('active');
+//     headerLinkShop.classList.add('active');
+//     firstLinkMobile.classList.remove('active');
+//     secondLinkMobile.classList.add('active');
+//   }
+// }
+
+// setActivePage(currentPageHash);
+
+// headerLinkHome.addEventListener('click', () => {
+//   setActivePage();
+// });
+
+// headerLinkShop.addEventListener('click', () => {
+//   setActivePage();
+// });
+
+//================================
+
 const currentPageHash = window.location.href;
 
 const headerLinkHome = document.querySelector('.site-nav-item-home');
@@ -19,7 +52,9 @@ function setActivePage() {
   }
 }
 
-setActivePage(currentPageHash);
+document.addEventListener('DOMContentLoaded', function () {
+  setActivePage();
+});
 
 headerLinkHome.addEventListener('click', () => {
   setActivePage();
